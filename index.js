9573f10e08920f6b00b0ae7b22e65eaa859a941d
Kévin ROBERT <kevin.robert.kr@gmail.com>
2026-03-28 00:29:43 +0100

    Init

---
+++ index.js
@@ -0,0 +1,38 @@
+import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { createBaseConfig } from '@krosoft/tooling-eslint';
+
+/**
+ * @param {{ tsconfigRootDir: string, project?: string[] }} options
+ * @returns {import("typescript-eslint").ConfigArray}
+ */
+export function createReactConfig(options) {
+  return tseslint.config(
+    ...createBaseConfig(options),
+    {
+      files: ['**/*.{ts,tsx}'],
+      plugins: {
+        react,
+        'react-hooks': reactHooks,
+        'react-refresh': reactRefresh,
+      },
+      settings: {
+        react: { version: 'detect' },
+      },
+      rules: {
+        ...react.configs.recommended.rules,
+        ...react.configs['jsx-runtime'].rules,
+        ...reactHooks.configs.recommended.rules,
+        'react-refresh/only-export-components': ['error', { allowConstantExport: true }],
+        'react/prop-types': 'off',
+        'react/react-in-jsx-scope': 'off',
+        'react/jsx-no-leaked-render': 'error',
+        'react/jsx-no-useless-fragment': 'error',
+        'react/self-closing-comp': 'error',
+        'react/jsx-curly-brace-presence': ['error', { props: 'never', children: 'never' }],
+      },
+    },
+  );
+}
